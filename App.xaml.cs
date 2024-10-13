afd46226df12130c65d679c1eee019dc4c1a076d
Nurkeldi Baktybay <st67280@upce.cz>
2024-10-13 19:14:30 +0200

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SP_Nurkeldi_Baktybay
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
